7a59249a132ec813d2918c68e7cfe04b5fda0361
Ayoub <elpasha117@gmail.com>
2023-02-26 21:21:25 +0100

    last update 26/2

--- client/marinid.py
+++ client/marinid.py
@@ -1,3 +1,5 @@
+import requests
+import json
 import discord
 from discord.ext import tasks
 
@@ -5,12 +7,50 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 fileName= 'latest.log'
+infoFileName = 'info.log'
+serverIP = '34.175.106.153'
+
+roleID = '1075921667454419036'
 
 commandPrefix = '/'
 command = 'minecraft'
 token = 'MTA3ODA3NjMzMTg1NTcyODgwMA.G-qlre.xVYtjK9AGHTqjzvz1OvroRLccw3nDbqck9K3CU'
 
 bot = discord.Client(intents=intents)
+
+def readinfo_file():
+    with open(infoFileName) as f:
+        return f.read()
+
+def dump_info():
+    info = get_info()
+    with open(infoFileName, 'w') as f:
+        f.write(info)
+        f.close
+
+def get_server(serverIP):
+    response = requests.get('https://mcapi.us/server/status?ip=' + serverIP)
+    if response.status_code == 200:
+        data = response.json()
+    else :
+        return 'request failed'
+    return data
+
+def get_info():
+    data = get_server(serverIP)
+    serverStatus = ''
+    serverBoolStatus = data['online']
+    if serverBoolStatus == True:
+        serverStatus = 'The server is online'
+    else:
+        serverStatus = 'The server is not online'
+    motd = data['motd']
+    nowPlayers = str(data['players']['now'])
+    maxPlayers = str(data['players']['max'])
+    serverVersion = data['server']['name']
+
+    return serverStatus + '\n\nServer Name: Marinid Dynasty\nIP Address: ['+serverIP+']\nVersion: '+serverVersion+'\nGame Mode: Hard, pussy.\nPlayers Online: '+nowPlayers+'/'+maxPlayers+'\n\nDescription: \n\n' + motd    
+
 
 def filter_line(string):
     filtered = ''
@@ -48,8 +88,17 @@ def read_file(fileName):
 async def on_message(message):
     if message.author == bot.user:
         return
+    if message.content.startswith(commandPrefix + "mcinfo"):
+        global ctx_info
+        await message.channel.send(">>> "+ "<@&"+roleID +">")
+        await message.channel.send(">>> Initializing Marinid Bot ")
+        ctx_info = await message.channel.send("```" + readinfo_file() + "```")
+        while True:
+            dump_info()
+            await ctx_info.edit(content = "```" + readinfo_file() + "```")
     if message.content.startswith(commandPrefix + command):
         global ctx
+        await message.channel.send(">>> "+ "<@&"+roleID +">")
         await message.channel.send(">>> "+ "Initializing Marinid Bot")
         ctx = await message.channel.send("```" + read_file(fileName) + "```")
         while True:
